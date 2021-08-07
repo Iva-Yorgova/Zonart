f933b6229d06a85c999e8e4bf3942984c6d5c45d
Iva <samaya@mail.bg>
2021-08-07 16:45:24 +0200

    Header icons updates

--- ZonartUsers/wwwroot/js/site.js
+++ ZonartUsers/wwwroot/js/site.js
@@ -117,3 +117,12 @@ function myFunction() {
 }
 
 
+// Open the full screen search box
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+// Close the full screen search box
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+}
