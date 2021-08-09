9a7a05f5d4de365a6f628ff6f972e0fa93c7ff9b
Iva <samaya@mail.bg>
2021-08-09 12:17:50 +0200

    Unit Tests

---
+++ ZonartUsers.Tests/Controller/AboutControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZonartUsers.Controllers;
+using FluentAssertions;
+using MyTested.AspNetCore.Mvc;
+using Xunit;
+
+namespace ZonartUsers.Tests.Controller
+{
+    public class AboutControllerTest
+    {
+
+        [Fact]
+        public void OurStoryShouldReturnViewWithCorrectModel()
+        {
+            //Arrange
+            var aboutController = new AboutController();
+
+            //Act
+            var result = aboutController.OurStory();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
