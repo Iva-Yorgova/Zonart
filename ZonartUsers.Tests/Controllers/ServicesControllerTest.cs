d8dfe960848cc834f3663fdcc2731caf3fc7b17e
Iva <samaya@mail.bg>
2021-08-10 18:15:58 +0200

    Projects Views Tweaks

---
+++ ZonartUsers.Tests/Controllers/ServicesControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using ZonartUsers.Controllers;
+using ZonartUsers.Models.Services;
+using ZonartUsers.Services.Statistics;
+using ZonartUsers.Tests.Mocks;
+using ZonartUsers.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace ZonartUsers.Tests.Controllers
+{    
+    public class ServicesControllerTest
+    {
+        [Fact]
+        public void AllShouldReturnViewWithCorrectModel()
+        {
+            // Arrange
+            using var data = DatabaseMock.Instance;
+
+            data.Contacts.Add(new Contact { Email = "test", Message = "test", Name = "test" });
+            data.Templates.Add(new Template { Name = "test", Price = 100});
+            data.Templates.Add(new Template { Name = "test", Price = 50});
+            data.Orders.Add(new Order { Name = "test"});
+            data.Users.Add(new User { Email = "test", FullName = "test" });
+            data.SaveChanges();
+
+            var statsServ = new StatisticsService(data);
+
+            var controller = new ServicesController(statsServ);
+
+            var stats = statsServ.Total();
+
+            // Act
+            var result = controller.All();
+
+            // Assert
+            Assert.NotNull(result);
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var model = viewResult.Model;
+
+            var viewModel = Assert.IsType<ServicesViewModel>(model);
+
+            Assert.Equal(2, viewModel.TotalTemplates);
+
+            //MyController<ServicesController>
+            //    .Instance()
+            //    .Calling(c => c.All())
+            //    .ShouldReturn()
+            //    .View(new ServicesViewModel 
+            //    { 
+            //        TotalContacts = stats.TotalContacts,
+            //        TotalOrders = stats.TotalOrders,
+            //        TotalTemplates = stats.TotalTemplates,
+            //        TotalUsers = stats.TotalUsers
+            //    });
+        }
+    }
+}
---
+++ ZonartUsers.Tests/Controllers/TemplatesControllerTest.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZonartUsers.Controllers;
+using FluentAssertions;
+using MyTested.AspNetCore.Mvc;
+using Xunit;
+using ZonartUsers.Data;
+using ZonartUsers.Models.Contacts;
+using ZonartUsers.Data.Models;
+using System.Linq;
+using ZonartUsers.Tests.Mocks;
+using ZonartUsers.Models.Templates;
+using System.Collections.Generic;
+
+namespace ZonartUsers.Tests.Controllers
+{
+    public class TemplatesControllerTest
+    {
+        [Fact]
+        public void AllShouldReturnViewWithCorrectModel()
+        {
+            MyController<TemplatesController>
+                .Instance()
+                .Calling(c => c.All())
+                .ShouldReturn()
+                .View();
+        }
+
+
+        [Fact]
+        public void AllShouldReturnViewWithCorrectModel2()
+        {
+            // Arrange
+            using var data = DatabaseMock.Instance;
+
+            data.Templates.Add(new Template { Name = "test", Price = 100 });
+            data.Templates.Add(new Template { Name = "test", Price = 50 });
+            data.SaveChanges();
+
+            var controller = new TemplatesController(data);
+
+            // Act
+            var result = controller.All();
+
+            // Assert
+            Assert.NotNull(result);
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var model = viewResult.Model;
+
+            var viewModel = Assert.IsType<List<TemplateListingViewModel>>(model);
+
+            Assert.Equal(2, data.Templates.Count());
+
+        }
+
+
+        [Theory]
+        [InlineData(1)]
+        public void DetailsShouldReturnViewWithCorrectModel(int id)
+        {
+            // Arrange
+            using var data = DatabaseMock.Instance;
+
+            data.Templates.Add(new Template { Name = "test", Price = 100 });
+            data.SaveChanges();
+
+            var controller = new TemplatesController(data);
+
+            // Act
+            var result = controller.Details(id);
+
+            // Assert
+            Assert.NotNull(result);
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var model = viewResult.Model;
+
+            var viewModel = Assert.IsType<TemplateLayoutModel>(model);
+
+            Assert.Equal(1, data.Templates.Count());
+
+        }
+    }
+}
