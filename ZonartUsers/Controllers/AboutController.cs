71e0c029d9ae39a60d38b7c9464967a1d5537cf5
Iva <samaya@mail.bg>
2021-08-12 06:41:37 +0200

    Statistics Replacement

--- ZonartUsers/Controllers/AboutController.cs
+++ ZonartUsers/Controllers/AboutController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ZonartUsers.Models.Services;
+using ZonartUsers.Services.Statistics;
 
 namespace ZonartUsers.Controllers
 {
     public class AboutController : Controller
     {
+        private readonly IStatisticsService statistics;
+
+        public AboutController(IStatisticsService statistics)
+        {
+            this.statistics = statistics;
+        }
+
         public IActionResult OurStory()
         {
-            return View();
+            var totalStatistics = this.statistics.Total();
+
+            return View(new ServicesViewModel
+            {
+                TotalTemplates = totalStatistics.TotalTemplates,
+                TotalUsers = totalStatistics.TotalUsers,
+                TotalOrders = totalStatistics.TotalOrders,
+                TotalContacts = totalStatistics.TotalContacts
+            });
         }
+
+
     }
 }
