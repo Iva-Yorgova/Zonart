2884db1ef1a8df2bb73dfa2db199458b5101abdb
Iva-Yorgova <samaya@mail.bg>
2021-08-17 08:28:52 +0200

    FAQ page tweaks

---
+++ ZonartUsers/Models/Users/FileFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ZonartUsers.Models.Users
+{
+    public class FileFormModel
+    {
+        public IFormFile FormFile { get; set; }
+    }
+}
